37f6833cac83ef538c67cc77e31bc9a4af561934
Abhinav Kumar <abhinav20060199@gmail.com>
2026-07-16 11:51:04 +0530

    PRODIGY_ML_05

---
+++ calorie_mapping.py
@@ -0,0 +1,12 @@
+calories = {
+    "pizza": 285,
+    "burger": 295,
+    "ice_cream": 207,
+    "apple_pie": 237,
+    "donut": 452,
+    "fried_rice": 238,
+    "steak": 271,
+    "salad": 152,
+    "sushi": 200,
+    "sandwich": 250
+}
